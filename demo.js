e9ec3b7123c48b0ec11e8ed38b4b7e80bdc67e23
rupeshpatil1702 <rupeshpatil1702@gmail.com>
2024-08-25 11:56:26 +0530

    chapter 3

---
+++ demo.js
@@ -0,0 +1,13 @@
+const jsonString = '{"name": "Alice", "age": 30}';
+const obj = JSON.parse(jsonString);
+console.log(obj)
+console.log(obj.name); // Output: "Alice"
+console.log(obj.age); // Output: 30
+
+
+console.log("lfdsjdsljdlfjdljfjdl")
+
+const obj1 = { name: "Alice", age: 30 };
+const jsonString1 = JSON.stringify(obj1);
+
+console.log(jsonString1); // Output: '{"name":"Alice","age":30}'
--- index.js
+++ index.js
@@ -1,10 +1,56 @@
- const http = require('http')
- 
-//  const data = 
-const server = http.createServer((req, res) => {
-    console.log("server started");
-    res.setHeader("header","value of headerk")
-    res.end();
-})
+const fs = require("fs");
+const index = fs.readFileSync("index.html", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
 
-server.listen(8080)
+const express = require("express");
+const morgan = require('morgan');
+const server = express();
+
+//Middleware
+server.use(express.json()) // bodyParser
+// server.use(express.urlencoded()) // used when we sends data from form
+
+server.use(morgan('default'))
+server.use(express.static('public'));
+// server.use((req,res,next) => {
+//     console.log(req.method, req.ip, req.hostname, new Date(),req.get("User-Agent"));
+//     next()
+// })
+const auth = (req, res, next) => {
+  console.log(req.body);
+  if (req.body.password == "123") {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// API - Endpoint = Route
+server.get("/", auth, (req, res) => {
+   res.json({ type: "GET" });
+});
+server.post("/", auth, (req, res) => {
+  res.json({ type: "POST" });
+});
+server.put("/", (req, res) => {
+  res.json({ type: "PUT" });
+});
+server.delete("/", (req, res) => {
+  res.json({ type: "DELETE" });
+});
+server.patch("/", (req, res) => {
+  res.json({ type: "PATCH" });
+});
+
+server.get("/demo", (req, res) => {
+  res.status(201).send("<b>20002</b>");
+  // res.sendStatus(404)
+  // res.send("<h1>helow server</h1>")
+  //   res.sendFile("D:/Study/Node/index.html");
+  // res.json(products)
+});
+
+server.listen(8080, () => {
+  console.log("server started on port 8080");
+});
